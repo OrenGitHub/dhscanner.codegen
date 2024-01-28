81210d267fa27a56244b184b2c831e1fdd1a267e
OrenGitHub <tuna.is.good.for.you@gmail.com>
2024-01-28 08:42:29 +0200

    improved skeleton (not compiling) packages

--- src/Packages.hs
+++ src/Packages.hs
@@ -8,43 +8,78 @@ import qualified Ast
 data Packages
    = Packages
      {
-         explicitPackages :: Map Token.PackageName Package
-         implicitPackages :: Map FilePath Package
+         actualPackages :: Map Ast.Root Package
      }
      deriving ( Show )
 
 data Package
-   = Package
+   = PackageExplictlyDefined String PackageContent
+   | PackageImplictlyDefinedFromFilename PackageContent
+   deriving ( Show )
+
+data PackageContent
+   = PackageContent
      {
+         ast :: Set Ast.Root,
          classes :: ActualType.Classes,
          functions :: ActualType.Functions,
          variables :: ActualType.Variables
      }
      deriving ( Show )
 
--- | API for creating packages
-packager :: Asts -> Packages
-packager asts = packager' (decs asts)
-
-packager' :: [ Ast.Root ] -> Packages
-packager' [] = emptyCollectionOfPackages
-packager' (ast:asts) = addPackage (packager'' ast) (packager' asts)
-
-packager'' :: Ast.Root -> Packages
-packager'' ast = let astDefinedPackages = filterAstDecPackages (decs ast) in
-    case astDefinedPackages of
-        Nothing -> fromSingleImplicitPackage (filename ast)
-        Just astDefinedPackage -> fromSingleExplicitPackage astDefinedPackages
-
--- if more than one package was declared, return the first declaration
-filterAstDecPackages :: [ Ast.Dec ] -> Maybe Ast.DecPackage
-filterAstDecPackages [] = Nothing
-filterAstDecPackages ((Ast.DecPackageCtor astDecPackage):decs) = astDecPackage
-filterAstDecPackages (dec:decs) = filterAstDecPackages decs
-
-fromSingleImplicitPackage :: String -> Packages
-fromSingleImplicitPackage filename = Packages {
-    explicitPackages = emptyCollectionOfExplicitPackages
-    implicitPackages = fromList [ (filename, p) ]
+emptyPackageContent = PackageContent {
+    classes = ActualType.emptyClasses,
+    functions = ActualType.emptyFunctions,
+    variables = ActualType.emptyVariables
 }
+
+packager :: Asts -> Packages
+packager asts = let
+    packages = getExplictlyDefinedPackages asts
+    asts' = append1stAstDecPackage asts
+    in
+    zipThemTogether packages asts'
+
+-- | Create an empty package
+-- * either explictly defined in the Ast
+--
+-- * or implictly from the corresponding filename
+--
+packager'' :: Ast.Root -> Package
+packager'' ast = let firstAstDecPackage = firstAstDecPackage (actualAst ast) in
+    case firstAstDecPackage of
+        Nothing -> implicitPackageFromFilename (filename ast)
+        Just astDefinedPackage -> explicitPackage astDefinedPackage
+
+firstAstDecPackage :: [ Ast.Dec ] -> Maybe Ast.DecPackage
+firstAstDecPackage [] = Nothing
+firstAstDecPackage ((Ast.DecPackageCtor astDecPackage):decs) = astDecPackage
+firstAstDecPackage (dec:decs) = firstAstDecPackage decs
+
+append1stAstDecPackage' :: Ast.Root -> (Ast.Root, Maybe Ast.DecPackage)
+append1stAstDecPackage' ast = (ast, firstAstDecPackage (actualAst ast))
+
+append1stAstDecPackage :: [ Ast.Root ] -> [(Ast.Root, Maybe Ast.DecPackage)]
+append1stAstDecPackage asts = map append1stAstDecPackage' asts
+
+mkEmptyExplicitPackage :: String -> Package
+mkEmptyExplicitPackage s = PackageExplictlyDefined s emptyPackageContent
+
+getExplictlyDefinedPackages :: Asts -> Map String Package
+getExplictlyDefinedPackages asts = let
+    asts' = astsContent asts
+    astsDecPackages = catMaybes $ map firsAstDecPackageIfAny asts'
+    astsDecPackagesAsStrings = map astDecPackageToString astDecPackages
+    distinct = toList $ fromList astsDecPackagesAsStrings
+    in
+    Map.fromList $ map mkEmptyExplicitPackage distinct
+
+implicitPackageFromFilename :: String -> Package
+implicitPackageFromFilename filename = Package $
+    PackageImplicitlyDefinedFromFilename $
+        PackageContent {
+            classes = emptyCollectionOfActualTypeClasses,
+            functions = emptyCollectionOfActualTypeFunctions,
+            variables = emptyCollectionOfActualTypeVariables
+        }
 
