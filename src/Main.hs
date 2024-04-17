21bc394fba6061c25b9fc999540ec074206293e7
OrenGitHub <tuna.is.good.for.you@gmail.com>
2024-04-17 12:19:05 +0300

    :beetle: Fix ( #4 ) healthcheck codegen container

--- src/Main.hs
+++ src/Main.hs
@@ -14,13 +14,24 @@ import Data.Aeson()
 import Asts
 import CodeGen ( codeGen )
 
+-- general imports
+import GHC.Generics
+
+data Healthy = Healthy Bool deriving ( Generic )
+
+instance ToJSON Healthy where toJSON (Healthy status) = object [ "healthy" .= status ]
+
 data App = App
 
 mkYesod "App" [parseRoutes|
 /codegen HomeR POST
+/healthcheck HealthcheckR GET
 |]
 
 instance Yesod App
+
+getHealthcheckR :: Handler Value
+getHealthcheckR = returnJson $ Healthy True
 
 postHomeR :: Handler Value
 postHomeR = do
