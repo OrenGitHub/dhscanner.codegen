86bbb35fc3789584cb18fc6d00868d04272990c1
OrenGitHub <tuna.is.good.for.you@gmail.com>
2024-06-19 09:18:25 +0300

    Handle request body size too big

--- src/Main.hs
+++ src/Main.hs
@@ -28,7 +28,7 @@ mkYesod "App" [parseRoutes|
 /healthcheck HealthcheckR GET
 |]
 
-instance Yesod App
+instance Yesod App where maximumContentLength = \app -> (\anyRouteReally -> Just 8000000)
 
 getHealthcheckR :: Handler Value
 getHealthcheckR = returnJson $ Healthy True
